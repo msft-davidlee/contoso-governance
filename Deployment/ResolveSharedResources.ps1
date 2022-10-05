ce18b240b6f22072d1883674a882db29be68493c
David Lee <leedavid@microsoft.com>
2022-10-05 10:26:09 -0500

    initial checkin to create shared rg resources

---
+++ Deployment/ResolveSharedResources.ps1
@@ -0,0 +1,6 @@
+$ErrorActionPreference = "Stop"
+    
+# This is the rg where the application should be deployed
+$groups = az group list --tag ard-environment=prod | ConvertFrom-Json
+$appResourceGroup = ($groups | Where-Object { $_.tags.'ard-solution-id' -eq 'shared-services' }).name
+Write-Host "::set-output name=appResourceGroup::$appResourceGroup"
---
+++ EnableSharedManagedIdentity.ps1
@@ -0,0 +1,32 @@
+$ErrorActionPreference = "Stop"
+    
+$kv = (az resource list --tag ard-resource-id=shared-key-vault | ConvertFrom-Json)
+if ($LastExitCode -ne 0) {
+    throw "An error has occured. Unable to locate key vault."
+}
+
+if (!$kv) {
+    throw "Unable to find eligible shared key vault resource!"
+}
+
+$sharedResourceGroup = $kv.resourceGroup
+
+$mid = (az identity list -g $sharedResourceGroup | ConvertFrom-Json).id
+if ($LastExitCode -ne 0) {
+    throw "An error has occured. An error occured when getting managed identity from rg."
+}
+
+if (!$mid) {
+    Write-Host "Managed identity does not exist. Creating one now..."
+
+    $miName = "shared-managed-identity"
+    # Perform role assignment
+    $mid = az identity create --name $miName --resource-group $sharedResourceGroup --tags ard-resource-id=$miName | ConvertFrom-Json
+
+    az role assignment create --assignee-object-id $mid.principalId --role "Key Vault Secrets User" `
+        --scope $kv.id --assignee-principal-type ServicePrincipal
+
+    if ($LastExitCode -ne 0) {        
+        throw "An error has occured. Unable to create $miName."
+    }
+}
